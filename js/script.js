5fa364f879bb9af85edbdbb0df89ca2e8002a18e
MariyanKarakiev <karakiev74@abv.bg>
2023-03-29 18:21:08 +0300

    init

---
+++ js/script.js
@@ -0,0 +1,170 @@
+(function ($) {
+
+  "use strict";
+
+  // preloader
+  var initPreloader = async function () {
+    $(document).ready(function ($) {
+      $('body').addClass('loading');
+    });
+    $(window).load(function () {
+      $('.preloader').fadeOut();
+      $('body').removeClass('loading');
+    });
+  }
+
+  //Overlay Menu Navigation
+  var overlayMenu = async function () {
+
+    if (!$('.nav-overlay').length) {
+      return false;
+    }
+
+    var body = undefined;
+    var menu = undefined;
+    var menuItems = undefined;
+    var init = function init() {
+      body = document.querySelector('body');
+      menu = document.querySelector('.menu-btn');
+      menuItems = document.querySelectorAll('.nav__list-item');
+      applyListeners();
+    };
+    var applyListeners =await function applyListeners() {
+      menu.addEventListener('click', function () {
+        return toggleClass(body, 'nav-active');
+      });
+    };
+    var toggleClass =await function toggleClass(element, stringClass) {
+      if (element.classList.contains(stringClass)) element.classList.remove(stringClass); else element.classList.add(stringClass);
+    };
+    init();
+  }
+
+  var typewriter = async function () {
+    var TxtType = function (el, toRotate, period) {
+      this.toRotate = toRotate;
+      this.el = el;
+      this.loopNum = 0;
+      this.period = parseInt(period, 10) || 2000;
+      this.txt = '';
+      this.tick();
+      this.isDeleting = false;
+    };
+
+    TxtType.prototype.tick = function () {
+      var i = this.loopNum % this.toRotate.length;
+      var fullTxt = this.toRotate[i];
+
+      if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+      } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+      }
+
+      this.el.innerHTML = this.txt;
+
+      var that = this;
+      var delta = 200 - Math.random() * 100;
+
+      if (this.isDeleting) { delta /= 2; }
+
+      if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+      } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+      }
+
+      setTimeout(function () {
+        that.tick();
+      }, delta);
+    };
+
+    window.onload = function () {
+      var elements = document.getElementsByClassName('typewrite');
+      for (var i = 0; i < elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+          new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+      }
+    };
+  }
+
+  var portfolio_height = async function ()  {
+    var heightPortfolio =  document.querySelector(".portfolio-container");
+    var section =  document.querySelector(".portfolio-section");
+    var bg =  document.querySelector(".bg-image");
+    var main = document.querySelector(".main");
+    var footer = document.querySelector("footer");
+console.log((heightPortfolio.clientHeight + main.clientHeight))
+    bg.style.height = heightPortfolio.clientHeight + 'px';
+    section.style.height = heightPortfolio.clientHeight + 'px';
+    footer.style.top = (heightPortfolio.clientHeight + main.clientHeight) + 'px';
+  }
+
+  $(window).resize(function() {
+    portfolio_height();
+  });
+
+  $(document).ready(function () {
+
+    initPreloader();
+    portfolio_height();
+    overlayMenu();
+    typewriter();
+
+    Chocolat(document.querySelectorAll('.image-link'), {
+      imageSize: 'contain',
+      loop: true,
+    });
+
+  });
+
+})(jQuery);
+
+/*
+(function() {
+  var triggerBttn = document.getElementById( 'trigger-overlay' ),
+    overlay = document.querySelector( 'div.overlay' ),
+    closeBttn = overlay.querySelector( 'button.overlay-close' );
+    transEndEventNames = {
+      'WebkitTransition': 'webkitTransitionEnd',
+      'MozTransition': 'transitionend',
+      'OTransition': 'oTransitionEnd',
+      'msTransition': 'MSTransitionEnd',
+      'transition': 'transitionend'
+    },
+    transEndEventName = transEndEventNames[ Modernizr.prefixed( 'transition' ) ],
+    support = { transitions : Modernizr.csstransitions };
+ 
+  function toggleOverlay() {
+    if( classie.has( overlay, 'open' ) ) {
+      classie.remove( overlay, 'open' );
+      classie.add( overlay, 'close' );
+      var onEndTransitionFn = function( ev ) {
+        if( support.transitions ) {
+          if( ev.propertyName !== 'visibility' ) return;
+          this.removeEventListener( transEndEventName, onEndTransitionFn );
+        }
+        classie.remove( overlay, 'close' );
+      };
+      if( support.transitions ) {
+        overlay.addEventListener( transEndEventName, onEndTransitionFn );
+      }
+      else {
+        onEndTransitionFn();
+      }
+    }
+    else if( !classie.has( overlay, 'close' ) ) {
+      classie.add( overlay, 'open' );
+    }
+  }
+ 
+  triggerBttn.addEventListener( 'click', toggleOverlay );
+  closeBttn.addEventListener( 'click', toggleOverlay );
+});
+*/
