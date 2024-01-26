e50e6d5fbbe2e0a9e340185754414401e1a4b766
MariyanKarakiev <karakiev74@abv.bg>
2024-01-26 18:22:02 +0200

    Navbar bootstrapped. No styles gut

--- js/script.js
+++ js/script.js
@@ -5,7 +5,42 @@ let scrolled = false;
 let path = window.location.pathname;
 let page = path.split("/").pop();
 
+function overlayMenu() {
+    if (document.getElementsByClassName('.nav-overlay').length) {
+        return false;
+    }
 
+    var body = undefined;
+    var menu = undefined;
+    var menuItems = undefined;
+
+    var init = function init() {
+        body = document.querySelector('body');
+        menu = document.querySelector('.menu-btn');
+        menuItems = document.querySelectorAll('.nav__list-item');
+        applyListeners();
+    };
+
+    var applyListeners = function applyListeners() {
+
+        menu.addEventListener('click', function () {
+            return toggleClass(body, 'nav-active');
+        });
+
+        menuItems.forEach(i => {
+            if (i.textContent.trim() == "Контакти") {
+                i.addEventListener('click', function () {
+                    return toggleClass(body, "nav-active")
+                })
+            }
+        });
+
+    };
+    var toggleClass = function toggleClass(element, stringClass) {
+        if (element.classList.contains(stringClass)) element.classList.remove(stringClass); else element.classList.add(stringClass);
+    };
+    init();
+}
 function animateOnIntersect() {
     const observer = new IntersectionObserver((entries) => {
         entries.forEach(entry => {
