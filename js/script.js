e3580e0359f8c560da209f190c272e7801a4e51c
MariyanKarakiev <karakiev74@abv.bg>
2024-01-27 17:39:09 +0200

    Showing image gallery.

--- js/script.js
+++ js/script.js
@@ -6,6 +6,72 @@ let scrolled = false;
 let path = window.location.pathname;
 let page = path.split("/").pop();
 
+function populateGallery() {
+    const galleryContainer = document.getElementById("gallery");
+    galleryContainer.hidden=false;
+    const widths = [400, 800, 1200]
+    const pageName = 'portraits'
+    const href = 'https://ik.imagekit.io/ycbriiund/LetMePhoto/' + pageName
+    const href2 = 'images/' + pageName
+    let tallImgNumbers = []
+    let imgsCount = 0
+
+    //sets number of images to be requested and images with numbers in their names that are tall
+    switch (pageName) {
+        case "portraits":
+            imgsCount = 12
+            tallImgNumbers = [2, 4, 5, 8, 10]
+            break;
+        case "balls_weddings":
+            imgsCount = 9
+            tallImgNumbers = [2, 4, 7]
+            break;
+        case "events":
+            imgsCount = 11
+            tallImgNumbers = [9]
+            break;
+        case "others":
+            imgsCount = 0
+            tallImgNumbers = [2, 4, 7]
+            break;
+    }
+
+    function galleryElementFactory(galleryContainer) {
+        for (let i = imgsCount; 0 < i; i--) {
+
+            const imageCard = document.createElement('div');
+            const imageLink = document.createElement('a');
+            const image = document.createElement('img');
+
+            let srcsetArr = widths.map(w => `${href}/tr:w-${w}/img${i}.jpg ${w}w`)
+
+            let imageType = tallImgNumbers.includes(i) ? 'img-tall' : 'img-wide';
+            imageCard.classList = [`image-link ${imageType}`];
+
+            //sets class for chocolat usage
+            imageLink.classList = ["galleryImg"]
+            //for when optimised images are not available in chocolat
+            imageLink.href = `${href2}/img${i}.jpg`
+            //for delivery of optimised images in chocolat
+            imageLink.dataset.srcset = srcsetArr
+
+            //for lazy loading 
+            image.classList = ["lazy img-fluid"];
+            //for delivery of optimised images in gallery section
+            image.srcset = srcsetArr;
+            //for when optimised images are not available
+            image.src = `${href2}/img${i}.jpg`
+
+            imageLink.appendChild(image);
+            imageCard.appendChild(imageLink);
+            galleryContainer.appendChild(imageCard);
+        }
+    }
+
+    if (page !== "index.html") {
+        galleryElementFactory(galleryContainer);
+    }
+}
 function overlayMenu() {
     if (document.getElementsByClassName('.nav-overlay').length) {
         return false;
@@ -59,4 +125,5 @@ function animateOnIntersect() {
 
 document.addEventListener("DOMContentLoaded",()=>{
     animateOnIntersect();
+   
 });
